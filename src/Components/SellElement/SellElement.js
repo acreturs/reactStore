b3624650cfbe76f74fcf76b7d62e609f2a64d638
Alexander Cornett <ge42nir@mytum.de>
2022-12-13 10:00:41 +0100

    lol

--- src/Components/SellElement/SellElement.js
+++ src/Components/SellElement/SellElement.js
@@ -12,8 +12,8 @@ const useStyles = createUseStyles({
 wrapper:{
     backgroundColor: 'black',
     border: '1px solid grey',
-    minWidth: '13rem',
-    minHeight:'13rem',
+    minWidth: '12rem',
+    minHeight:'12rem',
     margin: '3rem',
     textAlign: 'center',
     boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 2px 13px 0 rgba(0, 0, 0, 0.19)',
